316531430cbd2f9025eade13af0deb6a8add4b50
MKSparo <youfiff139@gmail.com>
2026-03-26 22:08:38 +0300

    Fix: Remove invalid next.config options for cleaner build

--- next.config.mjs
+++ next.config.mjs
@@ -10,10 +10,8 @@ const nextConfig = {
     // !! WARN !!
     ignoreBuildErrors: true,
   },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
+  typescript: {
+    ignoreBuildErrors: true,
   },
 };
 
